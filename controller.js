333e664e1ba155ffc8f825b3913d42c9383e21c6
Harry Vu <harryvu801@gmail.com>
2017-03-29 16:31:21 -0600

    mini users 2

--- controller.js
+++ controller.js
@@ -1,10 +1,28 @@
 angular.module('userProfiles').controller('MainController', function($scope, mainService) {
 
-  $scope.getUsers = function() {
-  	$scope.users = mainService.getUsers();
+   $scope.currentPage = 2;
+
+   $scope.getUsers = function() {
+
+      var promise = mainService.getData($scope.currentPage);
+
+      promise.then(function(users) {
+        $scope.users = users.data;
+        $scope.total_pages = users.total_pages;
+      })
+
   }
 
+   $scope.getUsers();
 
-  $scope.getUsers();
+   $scope.prev = function () {
+     $scope.currentPage--;
+     $scope.getUsers();
+  }
+
+   $scope.next = function () {
+      $scope.currentPage++;
+      $scope.getUsers();
+   }
 
 });
